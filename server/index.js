c1eb338e069738da3a278fca4b6b6c9af091eba5
Whyakshat <s24bcau0014@bennett.edu.in>
2026-07-03 00:42:18 +0530

    Fix prisma connection for Render

--- server/index.js
+++ server/index.js
@@ -9,10 +9,19 @@ import meetupsRoutes from './routes/meetups.js';
 import requestsRoutes from './routes/requests.js';
 import messagesRoutes from './routes/messages.js';
 
+// Naye Prisma v7 adapters
+import pg from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+
 dotenv.config();
 
+// Database se connect karne ka naya tareeqa
+const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
 const app = express();
-export const prisma = new PrismaClient();
+export const prisma = new PrismaClient({ adapter });
 
 app.use(cors());
 app.use(express.json({ limit: '50mb' })); // for large image uploads
@@ -25,7 +34,6 @@ app.use('/api/requests', requestsRoutes);
 app.use('/api/messages', messagesRoutes);
 
 const PORT = process.env.PORT || 5000;
-
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
